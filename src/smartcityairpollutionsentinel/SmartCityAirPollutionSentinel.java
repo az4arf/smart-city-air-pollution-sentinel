06bc10d648ec3eca94d0356aa3de91bbb83a3d4f
az4arf <azharfawzi004@gmail.com>
2026-05-15 22:12:54 +0200

    first commit

---
+++ src/smartcityairpollutionsentinel/SmartCityAirPollutionSentinel.java
@@ -0,0 +1,12 @@
+
+package smartcityairpollutionsentinel;
+
+
+public class SmartCityAirPollutionSentinel {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
